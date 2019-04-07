cbe04140a04862228fdd44ca7ffe83261d1b87b8
Rad <chihaby@gmail.com>
2019-04-07 00:56:09 -0700

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,8 @@ var path = require("path");
 module.exports = function (app) {
   //Load index page
   app.get("/", function (req, res) {
-    res.render("index");
+    res.render("index.handlebars");
   });
-
-
-
-
 
 
   // Load example page and pass in an example by id
