debea2b8713243f8c0c6c8fd023235f971c094d4
Rad <chihaby@gmail.com>
2019-04-07 11:30:17 -0700

    fixes routing error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   //Load index page
   app.get("*", function (req, res) {
     res.render("index");
-  }
+  });
 
 
   // Load example page and pass in an example by id
