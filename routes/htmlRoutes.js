9ff135c2b254cb99c7fe3e2aee1450062663fac9
Radouane <38984550+Radouane@users.noreply.github.com>
2019-04-09 06:20:05 -0700

    'update'

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ var db = require("../models");
 var path = require("path");
 module.exports = function (app) {
   //Load index page
-  app.get("/", function (req, res) {
-    res.render("index");
-  });
+  // app.get("/", function (req, res) {
+  //   res.render("index");
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
