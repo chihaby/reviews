efd0a63d98eb1896c09e7a575f06a4f1d8208415
Radouane <38984550+Radouane@users.noreply.github.com>
2019-04-09 06:40:31 -0700

    'update7'

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "test") {
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function () {
-  http.listen((process.env.PORT || 5000), function () {
+  app.listen((process.env.PORT || 5000), function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
